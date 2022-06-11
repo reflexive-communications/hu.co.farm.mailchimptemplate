9595a2792a116fab170aa393cd30c2189697027e
law909 <balint.lovey@gmail.com>
2022-06-11 16:40:54 +0200

    uprgade civix to 22.05.2

--- mailchimptemplate.php
+++ mailchimptemplate.php
@@ -13,18 +13,6 @@ function mailchimptemplate_civicrm_config(&$config)
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mailchimptemplate_civicrm_xmlMenu(&$files)
-{
-    _mailchimptemplate_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -82,60 +70,6 @@ function mailchimptemplate_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mailchimptemplate_civicrm_managed(&$entities)
-{
-    _mailchimptemplate_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailchimptemplate_civicrm_caseTypes(&$caseTypes)
-{
-    _mailchimptemplate_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailchimptemplate_civicrm_angularModules(&$angularModules)
-{
-    _mailchimptemplate_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mailchimptemplate_civicrm_alterSettingsFolders(&$metaDataFolders = null)
-{
-    _mailchimptemplate_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_preProcess().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
@@ -179,4 +113,22 @@ function mailchimptemplate_civicrm_navigationMenu(&$params)
             'active' => 1,
         ],
     ];
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function mailchimptemplate_civicrm_postInstall() {
+  _mailchimptemplate_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function mailchimptemplate_civicrm_entityTypes(&$entityTypes) {
+  _mailchimptemplate_civix_civicrm_entityTypes($entityTypes);
 }
