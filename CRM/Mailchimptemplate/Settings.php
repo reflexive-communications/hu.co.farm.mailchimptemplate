deb799764497dca6bc0556ec98569137a364a492
law909 <balint.lovey@gmail.com>
2022-06-12 02:45:21 +0200

    load/save settings

---
+++ CRM/Mailchimptemplate/Settings.php
@@ -0,0 +1,15 @@
+<?php
+
+class CRM_Mailchimptemplate_Settings {
+
+    private const SETTINGKEY = 'MailchimpTemplate_apikey';
+    public const ELFIELDNAME = 'apikey';
+
+    public static function getApikey() {
+        return CRM_RcBase_Setting::get(self::SETTINGKEY);
+    }
+
+    public static function setApikey($value) {
+        CRM_RcBase_Setting::saveSecret(self::SETTINGKEY, $value);
+    }
+}
