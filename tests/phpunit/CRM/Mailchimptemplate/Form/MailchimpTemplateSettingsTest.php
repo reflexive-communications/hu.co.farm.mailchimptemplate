8f44973e8c0cea202cee83e71247ac42816afa25
law909 <balint.lovey@gmail.com>
2022-07-08 01:59:07 +0200

    Settings form test

---
+++ tests/phpunit/CRM/Mailchimptemplate/Form/MailchimpTemplateSettingsTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use Civi\Test;
+use CRM_Mailchimptemplate_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+use Civi\Test\CiviEnvBuilder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test for the settings form
+ *
+ * @group headless
+ */
+class CRM_Mailchimptemplate_Form_MailchimpTemplateSettingsTest extends TestCase implements
+    HeadlessInterface,
+    HookInterface,
+    TransactionalInterface
+{
+
+    /**
+     * Setup used when HeadlessInterface is implemented.
+     *
+     * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+     *
+     * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+     *
+     * @return CiviEnvBuilder
+     *
+     * @throws CRM_Extension_Exception_ParseException
+     */
+    public function setUpHeadless(): CiviEnvBuilder
+    {
+        return Test::headless()
+            ->install('rc-base')
+            ->installMe(__DIR__)
+            ->apply();
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testPreProcess()
+    {
+        $form = new CRM_Mailchimptemplate_Form_MailchimpTemplateSettings();
+        $this->assertEmpty($form->preProcess());
+    }
+
+    public function testBuildQuickForm()
+    {
+        $form = new CRM_Mailchimptemplate_Form_MailchimpTemplateSettings();
+        $form->preProcess();
+        $this->assertEmpty($form->buildQuickForm());
+    }
+
+    public function testPostProcess()
+    {
+        $this->expectException(CRM_Core_Exception_PrematureExitException::class);
+        $apikey = 'postprocess-apikey';
+
+        $form = new CRM_Mailchimptemplate_Form_MailchimpTemplateSettings();
+        $form->preProcess();
+        $form->buildQuickForm();
+
+        $submitValues = [
+            CRM_Mailchimptemplate_Settings::ELFIELDNAME => $apikey
+        ];
+        $form->setVar('_submitValues', $submitValues);
+        $this->assertEmpty($form->postProcess());
+
+        $savedApiKey = CRM_Mailchimptemplate_Settings::getApikey();
+        $this->assertEquals($apikey, $savedApiKey, 'Form saved API key is wrong.');
+    }
+}
