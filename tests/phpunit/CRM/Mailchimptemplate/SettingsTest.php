2388eb62ed1f800c1555267b53cc3cd841090391
law909 <balint.lovey@gmail.com>
2022-07-13 02:26:04 +0200

    unneeded functions

--- tests/phpunit/CRM/Mailchimptemplate/SettingsTest.php
+++ tests/phpunit/CRM/Mailchimptemplate/SettingsTest.php
@@ -20,24 +20,6 @@ class CRM_Mailchimptemplate_SettingsTest extends TestCase implements HeadlessInt
     }
 
     /**
-     * The setup() method is executed before the test is executed (optional).
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    /**
-     * The tearDown() method is executed after the test was executed (optional).
-     *
-     * This can be used for cleanup.
-     */
-    public function tearDown(): void
-    {
-        parent::tearDown();
-    }
-
-    /**
      * Tests saving and loading settings
      */
     public function testSaveLoadApikey(): void
